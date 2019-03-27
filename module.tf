33b743dad28047cdd71a66357ef287622711f8e0
Jérôme Respaut <jerome.respaut@fr.clara.net>
2019-03-27 17:29:37 +0100

    AZ-22: Fixup Module subnet with NSG association

--- module.tf
+++ module.tf
@@ -7,7 +7,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${length(var.subnet_cidr) == 1 ? coalesce(element(var.custom_subnet_name, count.index), "${local.subnet_name}") : coalesce(element(var.custom_subnet_name, count.index), "${local.subnet_name}${count.index}")}"
   resource_group_name  = "${var.resource_group_name}"
   virtual_network_name = "${var.virtual_network_name}"
-  address_prefix       = "${element(var.subnet_cidr,count.index)}"
+  address_prefix       = "${element(var.subnet_cidr, count.index)}"
 
   route_table_id            = "${element(coalescelist(var.route_table_ids, list("")), count.index)}"
   network_security_group_id = "${element(coalescelist(var.network_security_group_ids, list("")), count.index)}"
@@ -17,12 +17,12 @@ resource "azurerm_subnet" "subnet" {
 
 resource "azurerm_subnet_network_security_group_association" "subnet_association" {
   count                     = "${length(var.network_security_group_ids)}"
-  subnet_id                 = "${azurerm_subnet.subnet.id}"
-  network_security_group_id = "${element(var.network_security_group_ids,count.index)}"
+  subnet_id                 = "${azurerm_subnet.subnet.*.id[count.index]}"
+  network_security_group_id = "${element(var.network_security_group_ids, count.index)}"
 }
 
 resource "azurerm_subnet_route_table_association" "route_table_association" {
   count          = "${length(var.route_table_ids)}"
-  subnet_id      = "${azurerm_subnet.subnet.id}"
-  route_table_id = "${element(var.route_table_ids,count.index)}"
+  subnet_id      = "${azurerm_subnet.subnet.*.id[count.index]}"
+  route_table_id = "${element(var.route_table_ids, count.index)}"
 }
