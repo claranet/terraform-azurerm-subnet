e72add7024c4440e90e2d78dbdd1b1033c407443
Jérôme Respaut <jerome.respaut@fr.clara.net>
2020-01-27 15:50:31 +0100

    AZ-171:  Update Splat expression to HCL2
    cf https://www.terraform.io/docs/configuration/expressions.html#splat-expressions

--- locals.tf
+++ locals.tf
@@ -2,5 +2,5 @@ locals {
   name_prefix = var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""
   subnet_name = "${local.name_prefix}${var.stack}-${var.client_name}-${var.location_short}-${var.environment}-subnet"
 
-  subnets_outputs = zipmap(azurerm_subnet.subnet.*.name, azurerm_subnet.subnet.*.id)
+  subnets_outputs = zipmap(azurerm_subnet.subnet[*].name, azurerm_subnet.subnet[*].id)
 }
