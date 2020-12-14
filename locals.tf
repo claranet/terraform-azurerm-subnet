b57829f7d1da957402db8d8e9105afa787f78d02
Rémi Beaufils <remi.beaufils@fr.clara.net>
2020-12-14 15:44:37 +0100

    AZ-398: Force lowercases on default generated name

--- locals.tf
+++ locals.tf
@@ -1,6 +1,8 @@
 locals {
-  name_prefix = var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""
-  subnet_name = "${local.name_prefix}${var.stack}-${var.client_name}-${var.location_short}-${var.environment}-subnet"
+  name_prefix  = var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""
+  default_name = lower("${local.name_prefix}${var.stack}-${var.client_name}-${var.location_short}-${var.environment}")
+
+  subnet_name = coalesce(var.custom_subnet_name, "${local.default_name}-subnet")
 
   subnets_outputs = zipmap(azurerm_subnet.subnet[*].name, azurerm_subnet.subnet[*].id)
 }
--- resources.tf
+++ resources.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "subnet" {
-  name                 = coalesce(var.custom_subnet_name, local.subnet_name)
+  name                 = local.subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = var.subnet_cidr_list
