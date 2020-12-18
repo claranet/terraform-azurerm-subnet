092f0941a180eb37075942f644cbc50bcff53328
Rémi Beaufils <remi.beaufils@fr.clara.net>
2020-12-18 16:29:11 +0100

    AZ-405: Replace join() by format()

--- locals.tf
+++ locals.tf
@@ -9,7 +9,7 @@ locals {
   network_security_group_rg = coalesce(var.network_security_group_rg, var.resource_group_name)
   route_table_rg            = coalesce(var.route_table_rg, var.resource_group_name)
 
-  network_security_group_id = join("", ["/subscriptions/", data.azurerm_subscription.current.subscription_id, "/resourceGroups/", local.network_security_group_rg, "/providers/Microsoft.Network/networkSecurityGroups/", coalesce(var.network_security_group_name, "fake")])
+  network_security_group_id = format("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/networkSecurityGroups/%s", "", data.azurerm_subscription.current.subscription_id, local.network_security_group_rg, coalesce(var.network_security_group_name, "fake"))
 
-  route_table_id = join("", ["/subscriptions/", data.azurerm_subscription.current.subscription_id, "/resourceGroups/", local.route_table_rg, "/providers/Microsoft.Network/routeTables/", coalesce(var.route_table_name, "fake")])
+  route_table_id = format("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/routeTables/%s", data.azurerm_subscription.current.subscription_id, local.route_table_rg, coalesce(var.route_table_name, "fake"))
 }
