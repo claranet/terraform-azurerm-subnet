b4233dfe2a81aedd24f3401a2d6838e017144690
Rémi Beaufils <remi.beaufils@fr.clara.net>
2020-12-18 16:02:19 +0100

    AZ-405: Fix issue with nsg and rt association dependencies + fix README

--- locals.tf
+++ locals.tf
@@ -5,4 +5,11 @@ locals {
   subnet_name = coalesce(var.custom_subnet_name, "${local.default_name}-subnet")
 
   subnets_outputs = zipmap(azurerm_subnet.subnet[*].name, azurerm_subnet.subnet[*].id)
+
+  network_security_group_rg = coalesce(var.network_security_group_rg, var.resource_group_name)
+  route_table_rg            = coalesce(var.route_table_rg, var.resource_group_name)
+
+  network_security_group_id = join("", ["/subscriptions/", data.azurerm_subscription.current.subscription_id, "/resourceGroups/", local.network_security_group_rg, "/providers/Microsoft.Network/networkSecurityGroups/", coalesce(var.network_security_group_name, "fake")])
+
+  route_table_id = join("", ["/subscriptions/", data.azurerm_subscription.current.subscription_id, "/resourceGroups/", local.route_table_rg, "/providers/Microsoft.Network/routeTables/", coalesce(var.route_table_name, "fake")])
 }
